4029ae313f192027ea8c53eeb9a6ae510866603f
Devin Chen <djchen@bu.edu>
2019-07-24 13:50:57 -0400

    update

--- keyboards/redox/rules.mk
+++ keyboards/redox/rules.mk
@@ -67,8 +67,6 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 RGBLIGHT_ENABLE = yes        # Enable WS2812 RGB underlight.  Do not enable this with audio at the same time.
-SUBPROJECT_rev1 = yes
-USE_I2C = yes
 
 SPLIT_KEYBOARD = yes
 
